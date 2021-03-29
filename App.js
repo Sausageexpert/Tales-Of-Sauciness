e07df60ce2327447d0aba703c15173690d74607f
Sausageexpert <68068569+Sausageexpert@users.noreply.github.com>
2021-03-29 16:53:03 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
-import ReadScreen from './Screens/ReadScreen';
-import WriteScreen from './Screens/WriteScreen';
+import OrderScreen from './Screens/OrderScreen';
+import ChooseScreen from './Screens/ChooseScreen';
 
 export default class App extends React.Component{
   render(){
@@ -13,8 +13,15 @@ export default class App extends React.Component{
 }
 
 const tabNavigator = createBottomTabNavigator({
-  WriteScreen: {screen: WriteScreen},
-  ReadScreen: {screen: ReadScreen}
+  OrderScreen: {screen: OrderScreen},
+  ChooseScreen: {screen: ChooseScreen}
 });
 
 const AppContainer = createAppContainer(tabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    background: 'lightblue'
+  }
+})
